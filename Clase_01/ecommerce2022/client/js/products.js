c2607ff5cae2febc52fb280f46ea87f1e195f1b7
Rodrigo Chavez <rodrigo.chavez.1986@gmail.com>
2023-09-06 16:42:49 -0300

    Clase 01 E-commerce Básico Parte 01

---
+++ Clase_01/ecommerce2022/client/js/products.js
@@ -0,0 +1,92 @@
+const productos = [
+    {
+        id:1,
+        productName:"Cafe",
+        price:600,
+        quantity:1,
+        img:"../media/cafe.png",
+
+    },
+
+    {
+        id:2,
+        productName:"Cafe con Leche",
+        price: 700,
+        quantity:1,
+        img:"../media/cafeconLeche.png",
+        
+    },
+
+    {
+        id:3,
+        productName:"Cookies",
+        price: 400,
+        quantity:1,
+        img:"../media/cookies.png",
+        
+    },
+
+    {
+        id:4,
+        productName:"Medias Lunas",
+        price: 300,
+        quantity:1,
+        img:"../media/mediaslunas.png",
+        
+    },
+
+    {
+        id:5,
+        productName:"Rolls de Canela",
+        price: 500,
+        quantity:1,
+        img:"../media/rolloscanela.png",
+        
+    },
+
+    {
+        id:6,
+        productName:"Tarta de Frutos",
+        price: 900,
+        quantity:1,
+        img:"../media/tartaFrutos.png",
+        
+    },
+
+    {
+        id:7,
+        productName:"Te",
+        price: 700,
+        quantity:1,
+        img:"../media/te.png",
+        
+    },
+
+    {
+        id:8,
+        productName:"Porción de Torta",
+        price: 1500,
+        quantity:1,
+        img:"../media/torta.png",
+        
+    },
+
+    {
+        id:9,
+        productName:"Tostadas Con Dulce",
+        price: 500,
+        quantity:1,
+        img:"../media/tostadas.png",
+        
+    },
+
+    {
+        id:10,
+        productName:"Tostado",
+        price: 1500,
+        quantity:1,
+        img:"../media/tostado.png",
+        
+    },
+
+];
